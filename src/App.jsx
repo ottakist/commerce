bd70d282d5bca39aad1c9bd237060b8faf1ee0e3
ottakist <102596775+ottakist@users.noreply.github.com>
2023-02-21 19:11:23 +0200

    Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+import {
+  Home,
+  About,
+  AuthWrapper,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute,
+  SingleProduct,
+  ProductsPage,
+} from './pages';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <h1>starter</h1>
-  )
+    <BrowserRouter>
+    <Navbar/>
+    <Sidebar/>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/products' element={<ProductsPage />} />
+        <Route path='/products/:id' element={<SingleProduct />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

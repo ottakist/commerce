021508834a04732eda6ef89c0787dddceac23a25
ottakist <102596775+ottakist@users.noreply.github.com>
2023-03-14 17:39:12 +0200

    mobile galari swipe

--- src/components/ProductImages.jsx
+++ src/components/ProductImages.jsx
@@ -1,11 +1,32 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useSwipeable } from 'react-swipeable';
 
 const ProductImages = ({ images = [{ url: '' }] }) => {
   const [mainImage, setMainImage] = useState(images[0]);
+  const [id, setId] = useState(images.indexOf(mainImage));
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => {
+      setId((prev) => (prev < images.length - 1 ? prev + 1 : (prev = 0)));
+      setMainImage(images[id]);
+    },
+    onSwipedRight: () => {
+      setId((prev) => (prev > 0 ? prev - 1 : (prev = images.length - 1)));
+      setMainImage(images[id]);
+    },
+    swipeDuration: 500,
+    preventScrollOnSwipe: true,
+    trackMouse: true,
+  });
   return (
     <Wrapper>
-      <img className='main' src={mainImage.url} alt={mainImage.filename} />
+      <img
+        className='main'
+        {...handlers}
+        src={mainImage.url}
+        alt={mainImage.filename}
+      />
       <div className='gallery'>
         {images.map((image, index) => {
           return (
